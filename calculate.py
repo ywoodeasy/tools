6d9d5e27f7c6fd2077e887df115d38b37c318d74
wood0618 <m202171043@hust.edu.cn>
2022-10-25 16:02:56 +0800

    增加两个def

--- calculate.py
+++ calculate.py
@@ -1,11 +1,20 @@
 def jia(a, b):
     return a+b
 
-
 def cha(a, b):
     if a >= b:
         return a-b
     else:
         return b-a
+
+def cheng(a, b):
+    return a*b
+
+def chu(a, b):
+    return a/b
+
+def jifen():
+    pass
+
 print(jia(1,3))
 print("修改一些东西")
